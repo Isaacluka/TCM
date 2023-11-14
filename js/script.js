ef7d4d81553baf9844783b7746e84ddedf38b6a7
Isaacluka <lukaisaacabdulkarim@gmail.com>
2023-11-14 17:12:16 +0100

    PreLaunch Commit 1

--- js/script.js
+++ js/script.js
@@ -37,3 +37,23 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 }
+//Countdown Timer
+var countDownDate = new Date("Nov 16, 2023 20:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance =countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60 )) / 1000 );
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("mins").innerHTML = minutes;
+    document.getElementById("secs").innerHTML = seconds;
+
+
+},1000)
+
+//
